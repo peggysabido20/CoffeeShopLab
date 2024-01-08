b60e73def62cb5a27da870394e2dfaa8847e783c
Peggy Sabido <peggy_sabido@hotmail.com>
2024-01-08 17:34:30 -0500

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoffeeShopLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShopLab.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
